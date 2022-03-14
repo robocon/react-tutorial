00c677fc313be3dd5f74a1a17bbd1aeae8983477
Kritsanasak Kuntaros <roboconk@gmail.com>
2022-03-14 21:27:39 +0700

    จับค่า Input กับ preventDefault form

--- src/components/FormComponent.js
+++ src/components/FormComponent.js
@@ -1,17 +1,33 @@
-const FormComponent = () => {
+import "./FormComponent.css"
+
+const FormComponent = () => { 
+    const inputTitle = (event)=>{
+        console.log(event.target.value)
+    }
+
+    const inputPrice = (event)=>{
+        console.log(event.target.value)
+    }
+
+    const saveItem = (event) =>{
+        event.preventDefault()
+        
+        console.log("บันทีกข้อมูลเรียบร้อย")
+    }
+
     return(
         <div>
-            <form>
+            <form method="POST" onSubmit={saveItem}>
                 <div className="form-control">
                     <label>ชื่อรายการ</label>
-                    <input type="text" name="title" placeholder="ระบุชื่อรายการ"/>
+                    <input type="text" name="title" placeholder="ระบุชื่อรายการ" onChange={inputTitle}/>
                 </div>
                 <div className="form-control">
                     <label>จำนวนเงิน</label>
-                    <input type="text" name="price" placeholder="ระบุจำนวนเงิน"/>
+                    <input type="number" name="price" placeholder="(+รายรับ, -รายจ่าย)" onChange={inputPrice}/>
                 </div>
                 <div>
-                    <button type="submit">บันทึก</button>
+                    <button type="submit" className="btn">บันทึก</button>
                 </div>
             </form>
         </div>
