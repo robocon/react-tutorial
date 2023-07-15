ca1c7e87752fb20b90ac68a552c3c90e31553e05
kritsanasak <roboconk@gmail.com>
2023-07-15 11:30:37 +0700

    ลองใช้งาน redux แป๊บหนึ่ง

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { v4 as uuidv4 } from 'uuid';
-import { useState,useEffect } from 'react';
+import { useState,useEffect, useReducer } from 'react';
 import Transection from './components/Transection';
 import FormComponent from './components/FormComponent';
 import DataContext from './data/DataContext';
@@ -19,12 +19,9 @@ function App() {
     ];
 
 	const css = {color:"red",textAlign:"center"};
-
 	const [items, setItems] = useState(initData);
-
 	const [reportIncome, setReportIncome] = useState(0);
 	const [reportExpense, setReportExpense] = useState(0);
-
 	const onAddNewItem = (newItem)=>{
 		// ข้อมูลที่ส่งมาจาก FormComponent.js
 		setItems((prevItem)=>{ // เอาข้อมูลเดิมที่อยู่ใน initdata ในที่นี้ตั้งชื่อเป็น prevItem 
@@ -42,7 +39,32 @@ function App() {
 		setReportExpense(expense);
 	},[items, reportIncome, reportExpense])
 
+	// กำหนดให้ count เป็น 0 เมื่อเริ่มต้น
+	const [count,setCount] = useState(0);
+
+	// reducer เป็นเหมือน function ตัวหนึ่งที่ซึ่งบอกว่าภายใน reducer จะให้มีการทำงานยังไงบ้าง
+	// state		ก็คือ state
+	// action		หรือ dispatch เป็นตัวบอกว่าจะให้ take action ยังไงบ้าง
+	const reducer = (state,action) => { 
+		// ใช้ switch case หรือ if เป็นตัวแยก action
+		switch (action.type) {
+			case "ADD":
+				return state+action.payload;
+				
+			case "SUB":
+				return state-action.payload;
+				
+			case "CLEAR":
+				return 0;
+		}
+	}
+
+	// เรียกใช้ useReducer โดยโยน function reducer เข้าไป กับค่าของ count
+	// dispatch		จะถูกกำหนดจาก event ที่เกิดขึ้นภายใน element ด้านล่าง
+	// result		เป็นค่าที่ return ออกมาจาก reducer
+	const [result, dispatch] = useReducer(reducer,count);
 	return ( 
+
 		<DataContext.Provider value={
 			{
 				income: reportIncome,
@@ -55,8 +77,13 @@ function App() {
 				<div><FormComponent onAddItem={onAddNewItem} /></div>
 				<div><Transection items={items}/></div>
 			</div>
+			<div align="center">
+				<p>{result}</p>
+				<button onClick={()=>dispatch({type:"ADD",payload:10})}>เพิ่ม</button>
+				<button onClick={()=>dispatch({type:"SUB",payload:5})}>ลด</button>
+				<button onClick={()=>dispatch({type:"CLEAR"})}>ล้าง</button>
+			</div>
 		</DataContext.Provider>
-		
 	);
 }
 
